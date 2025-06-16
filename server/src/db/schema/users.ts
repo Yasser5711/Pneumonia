2098094c2ea5d4c310fc0f40442840a7f59051ce
Yasser Mekhfi <75505765+Yasser5711@users.noreply.github.com>
2025-06-16 15:26:34 +0200

    feat(server): refactor apiKeys and users table definitions (#41)
    * feat(server): refactor apiKeys and users table definitions
    
    * fixup! feat(server): refactor apiKeys and users table definitions

--- server/src/db/schema/users.ts
+++ server/src/db/schema/users.ts
@@ -1,9 +1,9 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+import { pgTable } from 'drizzle-orm/pg-core';
 
-export const usersTable = pgTable('users', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-  phone: varchar({ length: 20 }),
-});
+export const usersTable = pgTable('users', (t) => ({
+  id: t.integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: t.varchar({ length: 255 }).notNull(),
+  age: t.integer().notNull(),
+  email: t.varchar({ length: 255 }).notNull().unique(),
+  phone: t.varchar({ length: 20 }),
+}));

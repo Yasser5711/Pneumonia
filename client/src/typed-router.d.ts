adc420a7ef8a0696880491fb574a1186e0615234
Yasser Mekhfi <75505765+Yasser5711@users.noreply.github.com>
2025-08-17 05:14:15 +0200

    feat: add turbo (#57)

--- client/src/typed-router.d.ts
+++ client/src/typed-router.d.ts
@@ -25,4 +25,52 @@ declare module 'vue-router/auto-routes' {
     'SignIn': RouteRecordInfo<'SignIn', '/signin', Record<never, never>, Record<never, never>>,
     'SignUp': RouteRecordInfo<'SignUp', '/signup', Record<never, never>, Record<never, never>>,
   }
+
+  /**
+   * Route file to route info map by unplugin-vue-router.
+   * Used by the volar plugin to automatically type useRoute()
+   *
+   * Each key is a file path relative to the project root with 2 properties:
+   * - routes: union of route names of the possible routes when in this page (passed to useRoute<...>())
+   * - views: names of nested views (can be passed to <RouterView name="...">)
+   *
+   * @internal
+   */
+  export interface _RouteFileInfoMap {
+    'src/pages/IndexPage.vue': {
+      routes: 'IndexPage'
+      views: never
+    }
+    'src/pages/404.vue': {
+      routes: 'NotFound'
+      views: never
+    }
+    'src/pages/ChatApp.vue': {
+      routes: 'ChatPage'
+      views: never
+    }
+    'src/pages/OAuthCallback.vue': {
+      routes: '/OAuthCallback'
+      views: never
+    }
+    'src/pages/SignInPage.vue': {
+      routes: 'SignIn'
+      views: never
+    }
+    'src/pages/SignUpPage.vue': {
+      routes: 'SignUp'
+      views: never
+    }
+  }
+
+  /**
+   * Get a union of possible route names in a certain route component file.
+   * Used by the volar plugin to automatically type useRoute()
+   *
+   * @internal
+   */
+  export type _RouteNamesForFilePath<FilePath extends string> =
+    _RouteFileInfoMap extends Record<FilePath, infer Info>
+      ? Info['routes']
+      : keyof RouteNamedMap
 }
